f6b94e1e86205dd7e7d390a5648cd7154358623a
Danil Khanalainen <atrenes@tune-it.ru>
2023-11-09 17:25:13 +0300

    exclude from test coverage

--- src/main/java/com/example/gogo/GogoApplication.java
+++ src/main/java/com/example/gogo/GogoApplication.java
@@ -1,9 +1,11 @@
 package com.example.gogo;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Generated
 public class GogoApplication {
 
     public static void main(String[] args) {
